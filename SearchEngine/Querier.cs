ce9b8405ffc391342da23ae854df877cbf8878f1
thompson <thompsonmina7@gmail.com>
2021-08-29 16:24:33 +0100

    querier in transit

---
+++ SearchEngine/Querier.cs
@@ -0,0 +1,15 @@
+using System;
+using System.IO;
+using System.Transactions;
+using System.Linq;
+using System.Collections.Generic;
+using LiteDB;
+
+namespace SearchEngine
+{
+    class Querier{
+        private Engine engine;
+        private Autocomplete autocomplete;
+
+    }
+}
