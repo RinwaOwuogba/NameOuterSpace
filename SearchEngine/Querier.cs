0eb398eb048772c3f0b534b230fab5a1a0676f73
Azur_xx <abdulmalik@adzymic.co>
2021-08-30 09:52:05 +0100

    Querier

--- SearchEngine/Querier.cs
+++ SearchEngine/Querier.cs
@@ -10,6 +10,9 @@ namespace SearchEngine
     public class Querier{
         private Engine engine;
         private Autocomplete autocomplete;
+        private ParsedQuery parser;
+        private Indexer indexer;
+
 
         public Querier(Engine eng){
             engine = eng;
@@ -18,6 +21,20 @@ namespace SearchEngine
         public string[] GetCompletions(string word){
             autocomplete = new Autocomplete(engine.GetAllWords());
             return autocomplete.auto(word);
+        }
+
+        public void Query(string query){
+            this.indexer =  new Indexer(this.engine.getmetainfo.stopwords);
+            this.parser = new ParsedQuery(query, this.indexer);
+            var ranker = new Ranker(this.parser, this.engine);
+            ranker.Rank();
+            var ranks = ranker.documentRanks;
+            var ids = new List<int>();
+            for(files in ranks){
+                ids.Add(files.Key);
+            }
+            var result = engine.GetDocuments(ids);
+
         }
         
 
